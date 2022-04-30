4b6e681372afa151ed0e5da3cdabdd5a915a43d2
a-palomino <76058124+a-palomino@users.noreply.github.com>
2022-04-30 17:41:23 +0200

    Goofy
    Subision de la pagina web de goofy xd

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+//This file contains all the functions required to the correct functionality of the site.
+
+function play_laught(){
+    const laught = document.getElementById('audio');
+
+    laught.play();
+}
